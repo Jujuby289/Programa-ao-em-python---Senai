e1bceb2ab9c6eb583f89b46415e000b7d23c38a0
Julia santos <resendejulia1985@gmail.com>
2026-06-12 17:57:32 -0300

    Add files via upload

---
+++ aula phyton.py
@@ -0,0 +1,15 @@
+n1 = int(input('digite um numero:'))
+n2 = int(input('digite um numero:'))
+
+
+soma = (n1 + n2)
+print(soma)
+
+substracao = (n1 - n2)
+print(substracao)
+
+divisao = (n1 / n2)
+print(divisao)
+
+fracao = (n1 + n2)
+print(fracao)
