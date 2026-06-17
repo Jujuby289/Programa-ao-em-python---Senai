afd8971e58f8266a25599fe3e20f9a774f5ee07c
Julia santos <resendejulia1985@gmail.com>
2026-06-17 14:47:55 -0300

    Add files via upload

---
+++ atividade 5.py
@@ -0,0 +1,49 @@
+#1
+print('sejam bem vindo')
+
+#2 
+valor = True
+print(valor)
+
+#3
+calculo = 5.0 * 5.69
+print(calculo)
+
+#4
+divisao = 5 / 5
+print(divisao)
+
+#5
+subtracao = 5 - 5
+print(subtracao)
+
+#6
+inteira = 5 // 5
+print(inteira)
+
+#7
+multiplicacao = 5 + 5 + 5 + 5
+print(multiplicacao)
+
+#8
+variavel = 5
+print(variavel + 5)
+
+#9
+print('ola, seja bem vindo!')
+
+print('vamos la!')
+nome = input('digite seu nome:')
+
+email = input('digite seu email:')
+
+idade = input('digite sua idade:')
+
+cidade = input('digite sua cidade:')
+
+graduacao = input('digite sua graduacao:')
+
+est = input('digite seu estado civil:')
+
+print('obrigado!!')
+
