1d3293180f47c6d82dc75156a4ac334633b6b530
Julia santos <resendejulia1985@gmail.com>
2026-06-19 17:55:51 -0300

    Add files via upload

---
+++ atividade.py
@@ -0,0 +1,28 @@
+#1
+
+positivo = int(input('digte um numero: '))
+
+if (positivo) > 0:
+    print('positivo') 
+elif (positivo) < 0:
+    print('negativo')
+
+#2
+
+idade = int(input('digte sua idade: '))
+
+if (idade) >= 18:
+    print('pode votar!') 
+elif (idade) < 18:
+    print('nao pode votar!')
+
+
+#3
+numero = 5
+if numero % 2 == 0:
+    print('o numero e par')
+else:
+    print('o numero e impar')
+  
+#4
+opcao_usuario = int
